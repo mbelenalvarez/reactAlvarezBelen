59c23382554a055d01cc047175e0f8ea644f76e4
Belen Alvarez <mbelenalvarez@ufasta.edu.ar>
2023-06-13 11:49:49 -0300

    primer commit

---
+++ react-alvarez-belen/src/components/CartWidget.js
@@ -0,0 +1,9 @@
+import React from "react"
+import { BsFillCartFill } from 'react-icons/bs'
+
+function CartWidget () {
+    return (
+    <h3> <BsFillCartFill /> </h3>)
+          }
+
+export default CartWidget
