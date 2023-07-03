f902b8672c5bd33cd59141e9fa890070d73c8476
Belen Alvarez <mbelenalvarez@ufasta.edu.ar>
2023-07-03 15:42:59 -0300

    sumando

--- react-alvarez-belen/src/components/ItemListContainer.js
+++ react-alvarez-belen/src/components/ItemListContainer.js
@@ -1,18 +1,23 @@
 import Box from '@mui/material/Box';
 import CardItem from './CardItem';
 
-const ItemListContainer = ({ items}) => {
+
+const ItemListContainer = ({ items, loading}) => {
     return (
       <Box display={'flex'} justifyContent={'center'} flexDirection={'row'} gap={5} flexWrap={'wrap'}>
-      {
+      <div style={containerStyle}>
+          {
+              Boolean(loading)        ?
+                  <p>cargando...</p>
+              :
           items.map((item, index) => {
               return (
                   <CardItem key={index + item.title} name={item.title} price={item.price} imageURL={item.image} description={item.description} />
               )
           })
-      }
+      } </div>
   </Box>
     )
-  }
+}
   
   export default ItemListContainer
